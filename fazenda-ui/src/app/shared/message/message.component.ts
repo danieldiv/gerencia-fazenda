6270976b895f5cfc0765b006180d8455c47955e9
danieldiv <das.29@outlook.com>
2022-09-29 14:47:53 -0300

    feat: nova funcao tem erro

--- fazenda-ui/src/app/shared/message/message.component.ts
+++ fazenda-ui/src/app/shared/message/message.component.ts
@@ -23,4 +23,8 @@ export class MessageComponent {
   @Input() error: string = '';
   @Input() contro?: AbstractControl | FormControl | null;
   @Input() text: string = '';
+
+  temErro() {
+    return this.contro ? this.contro.hasError(this.error) && this.contro.dirty : true;
+  }
 }
